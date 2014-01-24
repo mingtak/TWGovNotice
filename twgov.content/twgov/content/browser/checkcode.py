76a4836e0748de9de25ec98181a1a557c2a04579
andy fang <andy@mingtak.com.tw>
2014-01-24 12:21:12 +0000

    modify sendgovnotice.py, writelog format

--- twgov.content/twgov/content/browser/checkcode.py
+++ twgov.content/twgov/content/browser/checkcode.py
@@ -8,19 +8,30 @@ from ..config import PCC_DOMAIN
 from ..config import TEST_STRING
 from ..config import NOTICE_KEYWORDS
 from plone import api
-from random import randrange
+from random import randrange, random, choice
 from datetime import datetime
 
 
 class CheckCode(BrowserView):
     def __call__(self):
         catalog = api.portal.get_tool(name='portal_catalog')
-        brains = catalog(id='20140117150465070887')
+        brains = catalog(portal_type='twgov.content.govnotice')
         result = str()
         for brain in brains:
             item = brain.getObject()
-            if hasattr(item, 'organizationCode'):
-                result += '%s\t%s\n' % (brain.Title.decode('utf-8'), item.organizationCode)
+            item.viewPoint = random() * 100
+            if item.viewPoint < 35.0:
+                item.viewPoint += 35.0
+            item.budgetPoint = random() * 100
+            if item.budgetPoint < 35.0:
+                item.budgetPoint += 35.0
+            item.hotPoint = item.viewPoint * 0.35 + item.budgetPoint * 0.65
+            item.importantPoint = (item.viewPoint + item.budgetPoint + item.hotPoint) / 3
+            result += '%s\t%s\t%s\t%s\t%s\n' % (brain.Title.decode('utf-8'),
+                                        item.viewPoint,
+                                        item.budgetPoint,
+                                        item.hotPoint,
+                                        item.importantPoint,)
         return result
         
 #twgov.content.govnotice
